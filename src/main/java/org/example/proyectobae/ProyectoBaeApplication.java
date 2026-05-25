6d74bc11145f1cb9ff0428fd36562949d1688fd3
Victor Lerma <lermasanchezvictor@gmail.com>
2026-05-25 12:52:14 +0100

    Estructura basica

---
+++ src/main/java/org/example/proyectobae/ProyectoBaeApplication.java
@@ -0,0 +1,13 @@
+package org.example.proyectobae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoBaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/proyectobae/ProyectoBaeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.proyectobae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
